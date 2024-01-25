e3b0626f444e4a5ea962f0415b1384a1398449c1
Brian Smith <bsmith@axim.org>
2024-01-25 09:13:55 -0500

    check in dist for testing

---
+++ dist/App.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _jsxRuntime = require("react/jsx-runtime");
+const App = () => /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
+  "data-testid": "empty-app"
+});
+var _default = exports.default = App;
+//# sourceMappingURL=App.js.map
